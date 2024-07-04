9101715bc1bfa4826414578281395d06d79b8fc1
s1F10230108 <136304427+s1F10230108@users.noreply.github.com>
2024-07-04 14:44:37 +0900

    Update tests.py

--- todo/tests.py
+++ todo/tests.py
@@ -97,4 +97,4 @@ class TodoViewTestCase(TestCase):
         client = Client()
         response = client.get('/1/')
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, 404)
